220c8d94b9371bb625bfb82c202340ff815d86e0
Djan <124775935+DjanRamadan@users.noreply.github.com>
2023-04-05 12:05:46 +0300

    Add files via upload

---
+++ TestZad.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TestZad
+{
+    internal class TestZad
+    {
+        static void Main()
+        {
+            Dog myDog = new Dog();
+            myDog.Name = "Sharo";
+            myDog.Age = 3;
+            Console.WriteLine($"{myDog.Name} is {myDog.Age} years old");
+            myDog.Bark(5);
+        }
+    }
+
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Bark(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                Console.WriteLine($"Dog {Name} barks!");
+            }
+        }
+    }
+}
